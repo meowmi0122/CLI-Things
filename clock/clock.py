836e419162c1c6b6e470f899255139272deb3b40
meowmi0122 <meowmi0122@gmail.com>
2026-04-26 12:44:47 +0800

    Update clock.py

--- clock/clock.py
+++ clock/clock.py
@@ -1,56 +1,54 @@
-import os
-import time
-import shutil
-from datetime import datetime
+#!/bin/bash
 
-NUMS = {
-    "0": [" ████ ", "█    █", "█    █", "█    █", "█    █", "█    █", " ████ "],
-    "1": ["   █  ", "  ██  ", "   █  ", "   █  ", "   █  ", "   █  ", " █████"],
-    "2": [" ████ ", "     █", "     █", " ████ ", "█     ", "█     ", "█████ "],
-    "3": [" ████ ", "     █", "     █", " ████ ", "     █", "     █", " ████ "],
-    "4": ["█    █", "█    █", "█    █", "██████", "     █", "     █", "     █"],
-    "5": ["█████ ", "█     ", "█     ", "█████ ", "     █", "     █", "█████ "],
-    "6": [" ████ ", "█     ", "█     ", "█████ ", "█    █", "█    █", " ████ "],
-    "7": ["█████ ", "     █", "    █ ", "   █  ", "  █   ", "  █   ", "  █   "],
-    "8": [" ████ ", "█    █", "█    █", " ████ ", "█    █", "█    █", " ████ "],
-    "9": [" ████ ", "█    █", "█    █", " █████", "     █", "     █", " ████ "],
-    ":": ["      ", "  ██  ", "  ██  ", "      ", "  ██  ", "  ██  ", "      "]
+trap 'tput cnorm; clear; exit' INT TERM EXIT
+tput civis
+
+NUM_0=(" ████ " "█    █" "█    █" "█    █" "█    █" "█    █" " ████ ")
+NUM_1=("   █  " "  ██  " "   █  " "   █  " "   █  " "   █  " " █████")
+NUM_2=(" ████ " "     █" "     █" " ████ " "█     " "█     " "█████ ")
+NUM_3=(" ████ " "     █" "     █" " ████ " "     █" "     █" " ████ ")
+NUM_4=("█    █" "█    █" "█    █" "██████" "     █" "     █" "     █")
+NUM_5=("█████ " "█     " "█     " "█████ " "     █" "     █" "█████ ")
+NUM_6=(" ████ " "█     " "█     " "█████ " "█    █" "█    █" " ████ ")
+NUM_7=("█████ " "     █" "    █ " "   █  " "  █   " "  █   " "  █   ")
+NUM_8=(" ████ " "█    █" "█    █" " ████ " "█    █" "█    █" " ████ ")
+NUM_9=(" ████ " "█    █" "█    █" " █████" "     █" "     █" " ████ ")
+NUM_COLON=("      " "  ██  " "  ██  " "      " "  ██  " "  ██  " "      ")
+
+render() {
+    local text=$1
+    for i in {0..6}; do
+        line=""
+        for ((j=0; j<${#text}; j++)); do
+            c="${text:$j:1}"
+            case $c in
+                0) line+="${NUM_0[$i]}  " ;;
+                1) line+="${NUM_1[$i]}  " ;;
+                2) line+="${NUM_2[$i]}  " ;;
+                3) line+="${NUM_3[$i]}  " ;;
+                4) line+="${NUM_4[$i]}  " ;;
+                5) line+="${NUM_5[$i]}  " ;;
+                6) line+="${NUM_6[$i]}  " ;;
+                7) line+="${NUM_7[$i]}  " ;;
+                8) line+="${NUM_8[$i]}  " ;;
+                9) line+="${NUM_9[$i]}  " ;;
+                :) line+="${NUM_COLON[$i]}  " ;;
+            esac
+        done
+        echo "$line"
+    done
 }
 
-def render(text):
-    lines = [""] * 7
-    for c in text:
-        for i in range(7):
-            lines[i] += NUMS[c][i] + "  "
-    return lines
+while true; do
+    clear
+    render "$(date +"%H:%M:%S")"
+    sleep 1
 
-def draw_center(lines):
-    size = shutil.get_terminal_size()
-    rows, cols = size.lines, size.columns
+    read -rsn1 -t 0.001 key
+    if [ $? -eq 0 ]; then
+        break
+    fi
+done
 
-    h = len(lines)
-    w = max(len(l) for l in lines)
-
-    top = max((rows - h) // 2, 0)
-    left = max((cols - w) // 2, 0)
-
-    os.system("clear")
-
-    print("\n" * top, end="")
-    for l in lines:
-        print(" " * left + l)
-
-def clock():
-    while True:
-        now = datetime.now().strftime("%H:%M:%S")
-        lines = render(now)
-        draw_center(lines)
-        time.sleep(1)
-
-if __name__ == "__main__":
-    print("\033[?25l", end="")  # hide cursor
-    try:
-        clock()
-    finally:
-        print("\033[?25h", end="")  # restore cursor
-        os.system("clear")
+tput cnorm
+clear
